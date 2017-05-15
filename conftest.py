70cb18a4abacb5cfa25bd3d001d75c3f9e8be55a
Denis Averin <ddenex@gmail.com>
2017-05-16 00:39:22 +0700

    Add timeout to test server url

--- conftest.py
+++ conftest.py
@@ -19,6 +19,7 @@ import m3u8server
 
 TEST_SERVER_STDOUT = "tests/server.stdout"
 TEST_SERVER_URL = "http://{host}:{port}/path/to/redirect_me".format(**m3u8server.SERVER_ADDRESS)
+TEST_SERVER_STARTUP_TIMEOUT_SEC = 2.0
 
 
 @pytest.fixture(scope="session")
@@ -29,19 +30,22 @@ def m3u8_server(request):
                                           stdout=out_and_err_f, stderr=out_and_err_f)
         try:
             start_time = time.time()
-            while (time.time() - start_time) < 3.0:
+            while (time.time() - start_time) < TEST_SERVER_STARTUP_TIMEOUT_SEC:
                 try:
-                    u = urlopen(TEST_SERVER_URL)
+                    u = urlopen(TEST_SERVER_URL, timeout=(TEST_SERVER_STARTUP_TIMEOUT_SEC / 2))
                 except HTTPError:
                     break
                 except URLError as ue:
                     if ue.args[0].errno == 61:  # Connection refused because server is not ready yet
-                        time.sleep(0.2)
+                        time.sleep(TEST_SERVER_STARTUP_TIMEOUT_SEC / 10)
                         continue
                     raise
                 else:
                     u.close()
                     break
+            else:
+                raise Exception("Test server has not started in %.1f sec" % TEST_SERVER_STARTUP_TIMEOUT_SEC)
+            assert server_process.poll() is None, "Test server died"
             yield None
         finally:
             server_process.kill()
